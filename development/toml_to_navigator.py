8041909e39fb3bc247c9d6593b7476fc0bb00f27
Spacechrist <christianwallins@outlook.com>
2023-10-18 01:28:25 +0200

    Update toml_to_navigator.py

--- development/toml_to_navigator.py
+++ development/toml_to_navigator.py
@@ -94,7 +94,7 @@ total_techniques = 0
 for technique in techniques:
     total_techniques += 1
 
-output_path = r"C:\Users\Spacechrist\Documents\GitHub\detection-engineering\metrics\navigator.json"
+output_path = "metrics/navigator.json"
 
 outF = open(output_path, "w")
 outF.write(beginning)
